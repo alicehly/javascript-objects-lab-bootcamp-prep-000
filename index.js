beeb38b1c964e3fca3e2ad3c5a81d40fd13a44ef
Alice Ly Ms <alicehly@gmail.com>
2019-05-10 20:33:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes= new Object({});
 
-var object = {prop:1};
+const var object = {prop:1};
 
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, obj, {[key]: value});
