b887558311f873beeae447a4cc46b345dc1bed32
Alice Ly Ms <alicehly@gmail.com>
2019-05-10 20:37:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, obj, {[key]: value});
 }
 
+destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return 
+}
