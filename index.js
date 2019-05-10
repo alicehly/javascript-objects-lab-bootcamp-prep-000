8fc0655c73893205ac7df0475eef47dfe8733666
Alice Ly Ms <alicehly@gmail.com>
2019-05-10 20:47:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   delete object.key;
-  return object
+  return obj
 }
 
 function deleteFromObjectByKey(object, key) {
